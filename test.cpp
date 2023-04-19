d917a8582a7f6c40036b75dc0abc5a993d09a309
PiotrBzdrega <piotrbzdrega@yandex.com>
2023-04-19 07:54:41 +0200

    Tasks 1-3,6

--- test.cpp
+++ test.cpp
@@ -1,29 +1,110 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
+#include "test.h"
 
-uint32_t GetBitsFromUint32(uint32_t inputValue, uint8_t bitOffset, uint8_t Length);
 
 int main()
 {
 
 /* I used uintx_t types to be sure that this variable has actually x amount of bits */
-    uint32_t res = GetBitsFromUint32(0xABCDEF12, 4, 12);
-    std::vector<int> a={1,2,3};
-    std::cout<<&a[0]<<"\n";
-    std::cout<<&a[1]<<"\n";
-    std::cout<<&a[2]<<"\n";
-    printf("%08x ",res);
+    uint32_t res = GetBitsFromUint32(0xABCDEF12, 4, 12);//TASK1
+    printf("res: %08x \n",res);
+    uint32_t res1 = GetBitsFromUint32("0xaBCDEF12", 4, 12);//TASK2
+    printf("res1: %08x \n",res1);
+    
+ 
+ 
+    std::vector<bool> a = GetBitsVector(149); //TASK3
+    for (auto i:a)
+    {
+        std::cout<<i<<"\n";
+    }
+
+    std::vector<bool> g = GetBitsVector("0x95"); //TASK3
+    for (auto i:g)
+    {
+        std::cout<<i<<"\n";
+    }
+
+    std::cout<<"vector are same: "<<((a==g) ? true:false)<<"\n";
 
 
+
+    std::vector<char> b ={'a','b','c','d'};
+    PrintVectorRevers(b);//TASK6
+    
+    
     
     return 0;
+} 
+
+
+//TASK1
+uint32_t GetBitsFromUint32(uint32_t inputValue, uint8_t bitOffset, uint8_t Length)
+{   
+    // shift right + mask(2^Length)-1
+    uint32_t res=(inputValue>>bitOffset)&(1<<Length)-1;
+    return res;
 }
 
+//TASK2
+uint32_t GetBitsFromUint32(std::string inputValue, uint8_t bitOffset, uint8_t Length)
+{   
+    return GetBitsFromUint32(HexStringToInt(inputValue), bitOffset, Length);
+}
 
-
-uint32_t GetBitsFromUint32(uint32_t inputValue, uint8_t bitOffset, uint8_t Length)
+//auxiliary function for TASK2 and TASK3
+uint32_t HexStringToInt (std::string str)
 {
-    //uint32_t =
-    return inputValue>>bitOffset;
+    uint32_t res;
+
+    //loop from last to 2nd element ([0,1]=0x prefix)
+    for (size_t i = str.size()-1; i >1 ; i--)
+    {
+        //convert current element
+        uint8_t t=CharToInt(str[i]);
+
+        //iterator to track subsequent octet
+        int k=(str.size()-i-1);
+
+        //add following octet to result
+        res+=t<<(4*k);        
+    }
+
+    return res;
+}
+
+//auxiliary function for TASK2 and TASK3
+uint8_t CharToInt(const char c)
+{
+    //char to int using ASCII table
+    return  (c>'9') ? c -( c>'F' ? 'a' : 'A' )+10 : c-'0';
+}
+
+//TASK3
+std::vector<bool> GetBitsVector(uint32_t inputValue)
+{ 
+    std::vector<bool> res;
+    for (size_t i = 0; i < sizeof(uint32_t)*8; i++)
+    {   //check each bit, start from LSB
+        (inputValue &(0x1<<i)) ? res.push_back(true) : res.push_back(false);
+    }
+  return res;
+}
+
+//TASK3
+std::vector<bool> GetBitsVector(std::string inputValue)
+{
+    return GetBitsVector(HexStringToInt(inputValue));
+}
+
+//TASK6
+//TODO: template & better solution
+void PrintVectorRevers(std::vector<char> elements)
+{
+   if (elements.size()>0)
+    {
+        std::cout<< elements.back()<<"\n";
+        elements.pop_back();
+        return PrintVectorRevers(elements);
+    }       
+    
 }
---
+++ test.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+uint32_t GetBitsFromUint32(uint32_t inputValue, uint8_t bitOffset, uint8_t Length); //TASK1
+uint32_t GetBitsFromUint32(std::string inputValue, uint8_t bitOffset, uint8_t Length); //TASK2
+std::vector<bool> GetBitsVector(uint32_t inputValue); //TASK3
+std::vector<bool> GetBitsVector(std::string inputValue); //TASK3
+
+void PrintVectorRevers(std::vector<char> elements);//TASK6
+uint32_t HexStringToInt (std::string str); //auxiliary function for TASK2 and TASK3
+uint8_t CharToInt(const char c); //auxiliary function for TASK2 and TASK3
